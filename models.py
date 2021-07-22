bef03ffce297a4abd06380d5c92b976a628d6d05
Lincoln Lo <Lolincoln23@gmail.com>
2021-07-22 17:49:09 -0400

    Code clean up, added messages model, added single table inheritance

--- models.py
+++ models.py
@@ -7,28 +7,99 @@ class User(db.Model):
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), nullable=False)
-    age = db.Column(db.Integer(), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
     profile_link = db.Column(db.String(255))
     gender = db.Column(db.String(255), nullable=False)
     birthday = db.Column(db.DateTime())
     password_digest = db.Column(db.String(255))
-    rating = db.Column(db.Integer(), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
 
-    location = db.relationship('Location', backref='user', lazy=True, uselist=False)
-    # suite_id = db.Column(db.Integer)
+    suite_id = db.Column(db.Integer, db.ForeignKey('suites.id'))
+
+    location = db.relationship('UserLocation', backref='user', lazy=True, uselist=False)
+    setting = db.relationship('Setting', backref='user', lazy=True, uselist=False)
 
     created_on = db.Column(db.DateTime, server_default=db.func.now())
     updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-
-    def __repr__(self):
-        return f"{self.first_name} {self.last_name} {self.email}"
 
 
 class Location(db.Model):
     __tablename__ = 'locations'
     id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.Float(), nullable=False)
-    longitude = db.Column(db.Float(), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(20))
+
     created_on = db.Column(db.DateTime, server_default=db.func.now())
     updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'location',
+        "polymorphic_on": type
+    }
+
+
+class SuiteLocation(Location):
+    suite_id = db.Column(db.Integer, db.ForeignKey('suites.id'))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'suite_location'
+    }
+
+
+class UserLocation(Location):
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'user_location'
+    }
+
+
+class Setting(db.Model):
+    __tablename__ = 'settings'
+    id = db.Column(db.Integer, primary_key=True)
+    setting = db.Column(db.Text, nullable=False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+
+class Suite(db.Model):
+    __tablename__ = 'suites'
+    id = db.Column(db.Integer, primary_key=True)
+    active = db.Column(db.Boolean, nullable=False)
+    canvas = db.Column(db.Text)
+
+    users = db.relationship('User', backref='suite', lazy=True)
+    messages = db.relationship('SuiteMessage', backref='suite', lazy=True)
+    location = db.relationship('SuiteLocation', backref='suite', lazy=True, uselist=False)
+
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+
+class Message(db.Model):
+    __tablename__ = 'messages'
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(20))
+
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
+    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'message',
+        "polymorphic_on": type
+    }
+
+
+class SuiteMessage(Message):
+    suite_id = db.Column(db.Integer, db.ForeignKey('suites.id'))
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'suite_message'
+    }
+
+# class TaskMessage(db.Model):
--- seed.py
+++ seed.py
@@ -1,18 +1,59 @@
-from models import *
-from faker import Faker
 import random
+
+from faker import Faker
+
+from models import *
 
 faker = Faker()
 
 db.drop_all()
 db.create_all()
 
-for i in range(10):
-    location = Location(
+num_suites = 5
+for i in range(num_suites):
+    location = SuiteLocation(
         latitude=faker.latitude(),
         longitude=faker.longitude(),
     )
 
+    messages = []
+    for _ in range(3):
+        messages.append(SuiteMessage(
+            content=faker.text(),
+        ))
+
+    suite = Suite(
+        active=faker.boolean(),
+        canvas=faker.json(
+            data_columns={
+                'top_left': random.randrange(1, 100),
+                'top_right': random.randrange(1, 100),
+                'bottom_left': random.randrange(1, 100),
+                'bottom_right': random.randrange(1, 100),
+                'message': 'text'
+            },
+            num_rows=3
+        ),
+        location=location,
+        messages=messages
+    )
+
+    db.session.add(suite)
+
+for i in range(10):
+    location = UserLocation(
+        latitude=faker.latitude(),
+        longitude=faker.longitude(),
+    )
+
+    setting = Setting(
+        setting=faker.json(
+            data_columns={
+                'Version': '@1.0.1',
+                'Spec': {'theme': 'hex_color', 'language': 'language_name'}
+            },
+            num_rows=1)
+    )
     user = User(
         first_name=faker.first_name(),
         last_name=faker.last_name(),
@@ -23,9 +64,12 @@ for i in range(10):
         birthday=faker.date_of_birth(),
         password_digest=faker.password(length=255),
         rating=random.randrange(1, 10),
-        location=location
+        location=location,
+        setting=setting,
+        suite_id=i % num_suites + 1
     )
 
     db.session.add(user)
 
 db.session.commit()
+db.session.close()
