03ce5487bd435bfd688bc317d6beb1774db2f87f
Lincoln Lo <Lolincoln23@gmail.com>
2022-01-06 12:25:17 -0500

    fix comment

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ from flask_sqlalchemy import SQLAlchemy
 # Define the WSGI application object
 app = Flask(__name__)
 
-# Configurations
-app.config.from_object('config')
 
 # Define the database object which is imported
 # by modules and controllers.py
