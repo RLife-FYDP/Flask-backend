48c35041750b97d1577e18d6c81de13226533888
Austin Chen <itsaustinchen@gmail.com>
2022-01-31 21:58:30 -0500

    Fix db disconnect bug

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ DATABASE_PASSWORD = 'ceef9ffe'
 SQLALCHEMY_DATABASE_URI = f'mysql://b1af419355f3a4:{DATABASE_PASSWORD}@us-cdbr-east-05.cleardb.net/heroku_dc2cd1ae84a7237'
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+SQLALCHEMY_ENGINE_OPTIONS = { "pool_pre_ping": True, "pool_recycle": 300 }
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
 # incoming requests using one and performing background
