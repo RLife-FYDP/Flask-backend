cf98e7fbc1885d9276e77016e8f91eef95f40259
Lincoln Lo <Lolincoln23@gmail.com>
2022-02-10 21:59:25 -0500

    create update expense endpoint

--- app/expenses/controllers.py
+++ app/expenses/controllers.py
@@ -81,3 +81,24 @@ def get_expense(user, id):
         "expense_created_at": expense_item.created_at,
         "user_expenses": u_expense
     }
+
+@expenses.route('/<int:id>', methods=['PUT'])
+@authorize
+def update_expense(user, id):
+    expense_data = request.get_json()
+    expense_item = ExpenseItem.query.get_or_404(id)
+    expense_item.total_amount = expense_data['total_amount']
+    expense_item.description = expense_data['description']
+    expense_item.receipt_img_link = expense_data['receipt_img_link']
+    expense_item.paid_by_id = expense_data['paid_by_id']
+    expense_item.updated_at = datetime.datetime.now()
+    for user_expense_data in expense_data['user_expenses']:
+        user_expense = UserExpense.query.get((id, user_expense_data['user_id']))
+        user_expense.amount = user_expense_data['amount_owe']
+        user_expense.paid_at = user_expense_data['paid_at']
+    try:
+        db.session.commit()
+        return {"message": f"Updated expenses id {id}"}, 200
+    except Exception as err:
+        return {"message": "Error updating", "errors": str(err)}, 400
+
